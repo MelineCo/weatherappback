4e8d3d6463253866b42f4b3afd54f9ccdb6d5983
Emeline <emeline.courcelle@gmail.com>
2024-01-26 16:52:55 +0100

    first commit

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://melineco:wXeQSA3xvl2QlerI@cluster0.yci51gp.mongodb.net/weatherapp';
+const connectionString = process.env.CONNECTION_STRING;;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
